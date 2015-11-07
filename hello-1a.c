173cbce24ed67d685240db0e05088ae70df8d3e0
Michael Wehrley <michael.wehrley@gmail.com>
2015-11-06 19:45:21 -0600

    with user input via scan f

---
+++ hello-1a.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+  char c = 'm';
+  printf("hello %c\n", c);
+}
---
+++ hello-1b.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+  char input_c;
+  printf("Please input a character value: ");
+  scanf("%c", &input_c);
+  printf("hello %c\n", input_c);
+}
