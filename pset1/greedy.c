946e61f9ee25f8150ae6984c9e54e09138f281ea
Michael Wehrley <michael.wehrley@gmail.com>
2015-11-13 23:48:32 -0600

    removed unnecessary files

---
+++ pset1/greedy.c
@@ -0,0 +1,9 @@
+// jharvard@appliance (~/Dropbox/pset1): ./greedy
+// O hai! How much change is owed?
+// -0.41
+// How much change is owed?
+// -0.41
+// How much change is owed?
+// foo
+// Retry: 0.41
+// 4
