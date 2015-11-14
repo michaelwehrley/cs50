463928142dfd5dd386e958cbca56469789e75c2a
Michael Wehrley <michael.wehrley@gmail.com>
2015-11-14 01:03:01 -0600

    finished with integers

--- pset1/greedy.c
+++ pset1/greedy.c
@@ -17,32 +17,32 @@
 // 4
 
 #include  <stdio.h>
-#define QUARTER 0.25
-#define DIME 0.10
-#define NICKEL 0.05
-#define PENNY 0.01
-// #define COIN_TYPES = 4;
+#define QUARTER 25
+#define DIME 10
+#define NICKEL 5
+#define PENNY 1
 
 int main(void) {
-  float change_owed;
   int COIN_TYPES = 4;
-  int num_quarters = 0;
-  int num_dimes = 0;
-  int num_nickels = 0;
-  int num_pennies = 0;
-  float coins[4] = { QUARTER, DIME, NICKEL, PENNY };
-  int coin_count[4] = { num_quarters, num_dimes, num_nickels, num_pennies };
+  int LOWER_CHANGE_BOUND = 0;
+  int UPPER_CHANGE_BOUND = 10;
+  int CENTS_PER_DOLLAR = 100;
+  float change_owed;
+  int coin_count[4];
+  int coins[4] = { QUARTER, DIME, NICKEL, PENNY };
 
-  printf("O hai! How much change is owed?\n");
-  scanf(" %f", &change_owed);
-  
+  do
+  {
+    printf("O hai! How much change is owed?\n");
+    scanf("%f", &change_owed);
+  } while ((change_owed < LOWER_CHANGE_BOUND) || (change_owed > UPPER_CHANGE_BOUND));
+
+  change_owed = change_owed * CENTS_PER_DOLLAR;  
   
   for (int i = 0; i < COIN_TYPES; i++)
   {
     coin_count[i] = change_owed/coins[i];
-    printf("%i\n", coin_count[i]);
     change_owed = change_owed - coin_count[i] * coins[i];
-    printf("%f\n", change_owed);
   }
   printf("%i\n", coin_count[0] + coin_count[1] + coin_count[2] + coin_count[3]);
 }
