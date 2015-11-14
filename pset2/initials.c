a479ab3e7c37d4173221cc91a8c8a02bfaaf2f1b
Michael Wehrley <michael.wehrley@gmail.com>
2015-11-14 01:17:37 -0600

    adding directions

---
+++ pset2/initials.c
@@ -0,0 +1,10 @@
+// Write, in a file called initials.c, a program that prompts a user for their name (using GetString to obtain their name as a string) and then outputs their initials in uppercase with no spaces or periods, followed by a newline (\n) and nothing more. You may assume that the user’s input will contain only letters (uppercase and/or lowercase) plus single spaces between words. Folks like Joseph Gordon-Levitt, Conan O’Brien, and David J. Malan won’t be using your program.
+
+// So that we can automate some tests of your code, your program must behave per the examples below. Assumed that the underlined text is what some user has typed.
+
+// jharvard@appliance (~/Dropbox/pset2): ./initials
+// Zamyla Chan
+// ZC
+// jharvard@appliance (~/Dropbox/pset2): ./initials
+// robert thomas bowden
+// RTB
