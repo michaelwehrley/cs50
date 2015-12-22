16196057f754c4695766d6e628f85eb02618785b
Michael Wehrley <michael.wehrley@gmail.com>
2015-12-21 21:51:40 -0600

    * Fixed converting lower and upper by wrapping * Add more constants * Add helper functions

--- pset2/cesear.c
+++ pset2/cesear.c
@@ -7,44 +7,63 @@
 #include <string.h>
 #include <ctype.h>
 
+int const ALPHABET_LENGTH = 26;
+
+char decoded_char(char input, int upper_bound);
+
 int main(int argc, char const *argv[])
 {
-  if (argc != 2) {
+  int const NECESSARY_INPUTS = 2;
+  if (argc != NECESSARY_INPUTS) {
     printf("%s\n", "Whoops! Please add your cesear key.");
-    return 1;
   } else {
-    int count;
-    int cipher;
+    int cipher = atoi(argv[1]);
+    // fix this someday when I understand life.
+    char phrase[1000] = "";
+    if (cipher > 0) {
+      int k;
+      int length;
 
-    cipher = atoi(argv[1]);
-    if (cipher == 0 || cipher < 0)
-    {
-      printf("Try again: A cesear key must be a positive integer.\n");
-    }
-    else
-    {
-      int k = cipher % 26;
-
-      // fix this someday when I understand life.
-      char phrase[1000] = "";
-
+      k = cipher % ALPHABET_LENGTH;
       scanf("%[^\n]s", phrase);
-      count = strlen(phrase);
-      for (int i = 0; i < count; ++i)
-      {
-        if (isalpha(phrase[i]))
-        {
-          // upper
-          // lower
-          printf("%c", phrase[i] + k);
+      length = strlen(phrase);
+      for (int i = 0; i < length; ++i) {
+        char c;
+
+        if (isalpha(phrase[i])) {
+          int const ASCII_UPPER_Z = 90;
+          int const ASCII_LOWER_Z = 122;
+          char coded_char;
+
+          coded_char = phrase[i] + k;
+          if (isupper(phrase[i]))
+          {
+            c = decoded_char(coded_char, ASCII_UPPER_Z);
+          }
+          else
+          {
+            c = decoded_char(coded_char, ASCII_LOWER_Z);
+          }
         }
         else
         {
-          printf("%c", phrase[i]);
+          c = phrase[i];
         }
+        printf("%c", c);
       }
       printf("\n");
-      return 0;
     }
+    else
+    {
+      printf("Try again: A cesear key must be a positive integer.\n");
+    }
+  }
+}
+
+char decoded_char(char input, int upper_bound) {
+  if (input > upper_bound) {
+    return input - ALPHABET_LENGTH;
+  } else {
+    return input;
   }
 }
