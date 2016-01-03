4ecff4dea1589551be6688251c007d6ae4d98c50
Michael Wehrley <michael.wehrley@gmail.com>
2016-01-03 08:53:37 -0600

    fixed pointer issue with setting string literal to empty array

--- pset2/vigenere.c
+++ pset2/vigenere.c
@@ -1,49 +1,48 @@
-// jharvard@appliance (~/Dropbox/pset2): ./vigenere bacon
+// cipher_positionharvard@appliance (~/Dropbox/pset2): ./vigenere bacon
 // Meet me at the park at eleven am
 // Negh zf av huf pcfx bt gzrwep oz
-
+// Neg? zf av ?uf pc?x bt gzrwep oz
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
-int main(int const argc, char const *argv[])
-// find length of pointer value
-{
+int calc_cipher_length(char const *ptr);
+int cipher_position(int position, int const cipher_length);
+
+int main(int const argc, char const *argv[]) {
   int const NECESSARY_INPUTS = 2;
+  int const ALPHABET_LENGTH = 'z' - 'a' + 1;
   if (argc != NECESSARY_INPUTS) {
     printf("%s\n", "Whoops! Please add your vigenere cipher word.");
   } else {
-    // determine cipher word length
-    int cipher_length = 0;
-    int i = 0;
-    while (*(argv[1] + i)) {
-      cipher_length++;
-      i++;
-    }
+    int cipher_length = calc_cipher_length(argv[1]);
 
-    char message[] = "";
+    // `char message[] = "";` conflicts with address of `cipher_length`
+    char message[1000] = "";
     scanf("%[^\n]s", message);
     int message_length = strlen(message);
-    printf("%d\n", message_length);
-
-    for (int i = 0; i < message_length; ++i)
-    {
-      int change;
-      int j;
-      char letter;
-
-      if (i < cipher_length) {
-        j = i;
-      } else {
-        j = i % cipher_length;
-      }
-
-      change = *(argv[1] + j) - 'a' + 1;
+    int j = 0;
+    for (int i = 0; i < message_length; i++) {
+      int letter;
       if (isupper(message[i])) {
+        int position = cipher_position(j, cipher_length);
+        int change = *(argv[1] + position) - 'a';
         letter = message[i] + change;
+        if (letter > 'Z') {
+          letter = letter - ALPHABET_LENGTH;
+        }
+        j++;
+      } else if (islower(message[i])) {
+        int position = cipher_position(j, cipher_length);
+        int change = *(argv[1] + position) - 'a';
+        letter = message[i] + change;
+        if (letter > 'z') {
+          letter = letter - ALPHABET_LENGTH;
+        }
+        j++;
       } else {
-        letter = message[i] + change;
+        letter = message[i];
       }
       printf("%c", letter);
     }
@@ -51,3 +50,20 @@ int main(int const argc, char const *argv[])
   }
   return 0;
 }
+
+int calc_cipher_length(char const *ptr) {
+  int length = 0;
+  while (*(ptr + length)) {
+    length++;
+  }
+  return length;
+}
+
+int cipher_position(int position, int const cipher_length) {
+  if (position < cipher_length) {
+    position = position;
+  } else {
+    position = position % cipher_length;
+  }
+  return position;
+};
