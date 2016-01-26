88b2f1bf24dcdc5e18f31216f9be7c831dd2e852
Michael Wehrley <michael.wehrley@gmail.com>
2016-01-25 21:30:49 -0600

    correct implementation of swap function

--- pset3/find/test.c
+++ pset3/find/test.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int sort(int values[], int n);
 void show(int values[], int n);
@@ -13,26 +12,26 @@ int main(int argc, char const *argv[])
 
 int sort(int values[], int n) {
   // TODO: implement an O(n^2) sorting algorithm
-  int sorted_value = values[0];
+  show(values, n);
   int i = 0;
   while(i < n) {
     int new_value = values[i];
-    if (new_value < sorted_value) {
-      int* p_new_value = &new_value;
-      int* p_sorted_value = &sorted_value;
-      int first_value = sorted_value;
+    // i - 1
+    if (values[i] < values[0]) {
+      int temp = values[0];
+      int* p_new_value = &values[i];
+      int* p_sorted_value = &values[0];
+
       printf("value_a: %i\n", *p_sorted_value);
       printf("value_b: %i\n", *p_new_value);
       *p_sorted_value = new_value;
-      *p_new_value = first_value;
+      *p_new_value = temp;
       printf("value_a: %i\n", *p_sorted_value);
       printf("value_b: %i\n", *p_new_value);
     }
     i++;
   };
-  // show(values, n);
-  printf("0: %i\n", values[0]);
-  printf("1: %i\n", values[1]);
+  show(values, n);
   return 0;
 }
 
