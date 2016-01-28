b4885d95f36e1d1ba3faaa9e11929beba28ac17e
Michael Wehrley <michael.wehrley@gmail.com>
2016-01-28 11:40:32 -0600

    draft of 'divide_and_conquer'

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -6,7 +6,10 @@
  *
  * Helper functions for Problem Set 3.
  */
-       
+
+#include <stdio.h>
+#include <math.h>
+
 #include "cs50.h"
 
 #include "helpers.h"
@@ -14,20 +17,57 @@
 void bubble_sort(int values[], int n);
 void swap(int* p_new_value, int* p_sorted_value);
 
+void show(int values[], int n) {
+  printf("showing values: [");
+  int i = 0;
+  while(i < n) {
+    printf("%i, ", values[i]);
+    i++;
+  }
+  printf("]\n");
+}
+
 /**
  * Returns true if value is in array of n values, else false.
  */
+
+bool divide_and_conquer(int value, int n, int values[]) {
+  show(values, n);
+  int half = n / 2;
+  if (half == 0) {
+    return false;
+  } else if (value < values[half]) {
+    printf("%c\n", 'd');
+    printf("%i\n", half);
+    return divide_and_conquer(value, half, values);
+  } else if (value == values[half]) {
+    return true;
+  } else {
+    printf("%c\n", 'u');
+    int quarter = half / 2;
+    int middle = half + quarter;
+    printf("%i\n", middle);
+    return divide_and_conquer(value, middle, values);
+  }
+}
+
 bool search(int value, int values[], int n) {
   // TODO: implement a searching algorithm
-  int i = 0;
-  while(i < n) {
-    if (values[i] == value) {
-      return true;
-    }
-    i++;
-  }
-  return false;
+  sort(values, n);
+  return divide_and_conquer(value, n, values);
 }
+
+// bool search(int value, int values[], int n) {
+//   // TODO: implement a searching algorithm
+//   int i = 0;
+//   while(i < n) {
+//     if (values[i] == value) {
+//       return true;
+//     }
+//     i++;
+//   }
+//   return false;
+// }
 
 /**
  * Sorts array of n values.
